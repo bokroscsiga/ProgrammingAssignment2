d36dea4f9d96765ce7f0e96f7e939f59449dd6b1
bokroscsiga <bokroscsiga@gmail.com>
2014-08-24 21:04:42 +0200

    solution1

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The purpose of the following pair of functions is to save computation time
+## when the inverse of a matrix has to be computed repeteadly. 
+## The first function, "makeCachematrix" is used to creat a the matrix 
+## the inverse of which will be computed. 
+## The second function, "cacheSolve" accesses the matrix, if the inverse of it
+## has not been computed yet then this second function computes it, stores it, 
+## and returns it; and if it has already been calculated then "cacheSolve" 
+## picks it up and returns the inverse. 
 
-## Write a short comment describing this function
+## This function creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    I <- NULL                   # I will store the inverse of our matrix
+    set <- function(y) {
+        x <<- y
+        I <<- NULL
+    }
+    get <- function() x         # this function returns the original matrix
+    setinv <- function(inv) I <<- inv
+    getinv <- function() I
+    list(set = set, get = get,
+         setinv = setinv,
+         getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned by 
+## "makeCacheMatrix" above. If the inverse has already been calculated, 
+## then "cacheSolve" should retrieve the inverse from the cache.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...) { 
+    I <- x$getinv()             # accesses the matrix 'x' and gets the inverse
+    if(!is.null(I)) {           # if inverse was already computed
+        message("getting cached data")
+        return(I)
+    }
+    data <- x$get()
+    I <- solve(data, ...)
+    x$setinv(I)                 # store the calculated inverse in x
+    I
 }
+
